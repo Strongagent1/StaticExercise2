cc57058e9e5979b7d606ab466887b46d75c35306
Kyle Jenne <strongagent1@gmail.com>
2023-01-09 14:16:15 -0500

    Reworked Exercise - KJ

---
+++ Static-Exercise/StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace StaticExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The Celsius of 212F is : ");
+            Console.WriteLine(TempConverter.FahrenheitToCelsius(212));
+            Console.Write("The Fahrenheit of 100C is : ");
+            Console.WriteLine(TempConverter.CelsiusToFahrenheit(100));
+        }
+    }
+}
